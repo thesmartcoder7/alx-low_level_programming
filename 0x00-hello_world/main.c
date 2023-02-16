9bed1104f20293e48303d8509392a10d953733a7
Samuel Martins <samuel.martins4.sm.com>
2023-02-16 23:42:05 +0300

    chore: create a script that runs a C file through the preprocessor

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: This is a simple C function
+ * that does absolutely nothing
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    return (0);
+}
