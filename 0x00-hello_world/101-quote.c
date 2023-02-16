2ff7620ff91a4176abb7bc0b782eaca6159d56db
Samuel Martins <samuel.martins4.sm.com>
2023-02-17 02:11:50 +0300

    chore: prints stuff to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 ( success )
+ *
+ * Description: This program prints stuff to the standard error
+*/
+
+int main(void)
+{
+    printf('and that piece of art is useful" - Dora Korpar, 2015-10-19\n');
+    return (1);
+}
