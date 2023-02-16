b0b7fcde69b124d40fbe669674d41c94360a667f
Samuel Martins <samuel.martins4.sm.com>
2023-02-17 02:25:56 +0300

    chore: prints stuff to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,8 @@
 
 int main(void)
 {
-char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-write(1, quote, 59);
-return (1);
+    write(1, quote, 59);
+    return (1);
 }
