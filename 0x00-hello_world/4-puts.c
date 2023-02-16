30be0a58c9a0af9d01571ff5081309c68308de21
Samuel Martins <samuel.martins4.sm.com>
2023-02-17 01:44:12 +0300

    chore: create a function prints stuff

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 ( success )
+ * Description: A simple program that uses puts
+*/
+
+void main(void)
+{
+    puts("Programming is like building a multilingual puzzle\n");
+    return (0);
+}
