51181b12c4e19ec890d480f52714f86a98032a4e
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-02-21 20:35:16 +0300

    chore: create a function to print  text  without the standard library

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,14 +1,16 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
+ * @ch: characters
+ * description: The character to print
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+ *         On error, -1 is returned, and errno is set appropriately.
+*/
 
-int _putchar(char c)
+int _putchar(char ch)
 {
-	return (write(1, &c, 1));
+	putchar(ch);
+	return (1);
 }
