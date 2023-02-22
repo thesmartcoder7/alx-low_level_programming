692829663e6f10dbfa68019ff85baa03dd5dcb2a
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-02-22 04:41:50 +0300

    chore: print every minute of day from 00:00 ot 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,7 +3,7 @@
  * jack_bauer - Entry point
  *
  * Description: this is a program that prints every
- *		minute of the day of Jack Bauer, starting
+ *		min of the day of Jack Bauer, starting
  *		from 00:00 to 23:59
  *
  * Return: Always 0 (Success)
@@ -11,21 +11,21 @@
 
 void jack_bauer(void)
 {
-	int hour, minute;
+	int hr, min;
 
-	for (hour =  0; hour < 24; ++hour)
+	for (hr =  0; hr < 24; ++hr)
 	{
 
-		for (minute = 0; minute < 60; ++minute)
+		for (min = 0; min < 60; ++min)
 		{
 
-			_putchar ((hour / 10) + 48);
-			_putchar ((hour % 10) + 48);
+			_putchar ((hr / 10) + 48);
+			_putchar ((hr % 10) + 48);
 
 			_putchar (':');
 
-			_putchar ((minute / 10) + 48);
-			_putchar ((minute % 10) + 48);
+			_putchar ((min / 10) + 48);
+			_putchar ((min % 10) + 48);
 			_putchar ('\n');
 
 		}
