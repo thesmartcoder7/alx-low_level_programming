94cefa6ecab7683602c81c425ca5600832a27d0b
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-02-22 04:49:04 +0300

    chore: create a function that  adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - check the code.
+ * @first: input parameter
+ * @second: input parameter
+ *
+ * Description: this program adds two integers and returns the result.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int add(int first, int second)
+{
+	return (first + second);
+}
+
