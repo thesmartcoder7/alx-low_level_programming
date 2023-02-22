e4f8ca7b3a906412126e5c3a6994839d1ce00b95
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-02-22 04:16:44 +0300

    chore: create a program that checks for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Description: This is a program that prints the alphabet in lowercase
+ *
+ * @c: checks input of function
+ *
+ * Return: Always 0 ( success )
+*/
+
+int _islower(int c)
+{
+	return (c >= 98 && c <= 122);
+}
+
+
