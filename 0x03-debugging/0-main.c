2a26159fb418b78a82407608a74b36eb6b7f4e81
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-02-23 15:31:46 +0300

    chore: create a test file for the positive-negative function

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -10,11 +10,7 @@
 
 int main(void)
 {
-	if (positive_or_negative(0) == "0 is zero")
-		printf("Correct output");
-	else
-		printf("Incorrect output");
-
+	positive_or_negative(0)
 	return (0);
 }
 
