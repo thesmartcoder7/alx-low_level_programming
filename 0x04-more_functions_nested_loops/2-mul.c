045a88f7cfcde89488b5cb767f6e6c04c5a8a00a
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-02-24 03:57:34 +0300

    chore: create a function to multiply two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+/**
+ * mul - entry point to function
+ *
+ * @first: first input parameter
+ * @second: second input parameter
+ *
+ * Description: this program multiplies two integers
+ *
+ * Return: always (0)
+*/
+
+int mul(int first, int second)
+{
+	return (first * second);
+}
