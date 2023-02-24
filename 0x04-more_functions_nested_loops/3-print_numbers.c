280b331f68de1b28de84512628024b94c7d8d100
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-02-24 03:59:56 +0300

    chore: create a function to print numbers from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * print_numbers - entry point to function
+ *
+ * Description: this function prints numbers from 0-9
+ *
+ * Return: always (0)
+*/
+
+void print_numbers(void)
+{
+	int num = 0;
+
+	do {
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
+
+	_putchar('\n');
+}
