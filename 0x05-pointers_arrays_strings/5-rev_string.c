bbbfde24764a0df71290d9a3f05b6ad1eb1d9b94
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-02-28 03:32:48 +0300

    chore: creae a function that reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string parameter input
+ *
+ * Return: void
+*/
+
+void rev_string(char *s)
+{
+	int length, index;
+	char ch;
+
+	for (length = 0; s[length] != '\0'; ++length)
+		;
+
+	for (index = 0; index < length / 2; ++index)
+	{
+		ch = s[index];
+		s[index] = s[length - 1 - index];
+		s[length - 1 - index] = ch;
+	}
+
+}
