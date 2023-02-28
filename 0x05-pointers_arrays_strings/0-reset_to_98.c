ccad7d0642b2937fcbee4a8b8fe2ba176138c6c2
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-02-28 03:07:46 +0300

    chore: create a function to point to a specific value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+/**
+ * reset_to_98 - function name
+ *
+ * @n: integer parameter input
+ *
+ * Description: this function takes a pointer to an int
+ *		parameter and updates the value it points to 98
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
