1bee05bb6e8799fbeb1b52966464606654905b0b
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-03-11 02:42:00 +0300

    chore: add a file to print the name of the program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,9 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char **argv)
 {
-	printf("%s\n", argv[argc * 0]);
+	printf("%s\n", *argv);
 
 	return (0);
 }
