0d1f5df819aeae51856e5d189a86c3875306ac02
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-04-04 01:51:13 +0300

    chore: delete head of node and return node's data

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * pop_listint: delete the head node of a listint_t list.
+ *
+ * @head: pointer to the address of the
+ *        head of the listint_t list.
+ *
+ * Return: 0 if the list is empty . . .
+ *         else . . . the head node's data (n).
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int res;
+
+	if (*head == NULL)
+		return (0);
+
+	temp = *head;
+	res = (*head)->n;
+	*head = (*head)->next;
+
+	free(temp);
+
+	return (res);
+}
