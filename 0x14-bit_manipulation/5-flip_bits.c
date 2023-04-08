bf4e29b15f74bbd45e38e115d2048bbbf82c8f46
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-04-08 23:38:39 +0300

    chore: get the number of bits required to get from one number to another

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,8 +12,7 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int k = n ^ m
-	int bits_req = 0;
+	unsigned long int k = n ^ m, bits_req = 0;
 
 	while (k > 0)
 	{
