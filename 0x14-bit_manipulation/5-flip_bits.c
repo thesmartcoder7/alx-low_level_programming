428531acea1bd62967b1d0bcb14be3f44ccb506a
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-04-08 23:37:42 +0300

    chore: get the number of bits required to get from one number to another

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - this function gets the number of bits required
+ *             to get from one number to another.
+ *
+ * @n: The number.
+ * @m: The number to flip n to.
+ *
+ * Return: the number of bits flips to get from n to m.
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int k = n ^ m
+	int bits_req = 0;
+
+	while (k > 0)
+	{
+		bits_req += (k & 1);
+		k >>= 1;
+	}
+
+	return (bits_req);
+}
