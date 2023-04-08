a6ce8e627fd6db03486b136c5f4ae51136116a1e
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-04-08 23:49:45 +0300

    patch: update the binary change function

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,25 +2,32 @@
 
 /**
  * binary_to_uint - Converts a binary number to an unsigned int.
+ *
  * @b: input binary number.
+ *
  * Return: 0 if b is NULL or contains chars not 0 or 1 . . .
  *         else . . . it returns the converted number.
  */
 
-unsigned int binary_to_uint(const char *b)
+unsigned int binary_to_uint(const char *n)
 {
-	unsigned int smc_num = 0;
-	int sm_len = 0;
+	unsigned int sm_i, sm_p;
+	int sm_l;
 
-	if (b[sm_len] == '\0')
+	if (n == NULL)
 		return (0);
 
-	while ((b[sm_len] == '0') || (b[sm_len] == '1'))
+	for (sm_l = 0; n[sm_l]; sm_l++)
 	{
-		smc_num <<= 1;
-		smc_num += b[sm_len] - '0';
-		sm_len++;
+		if (n[sm_l] != '0' && n[sm_l] != '1')
+			return (0);
 	}
 
-	return (smc_num);
+	for (sm_p = 1, sm_i = 0, sm_l--; sm_l >= 0; sm_l--, sm_p *= 2)
+	{
+		if (n[sm_l] == '1')
+			sm_i += sm_p;
+	}
+
+	return (sm_i);
 }
