6080143888815ffe505a30dd5f552e054bfd0577
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-04-08 23:01:28 +0300

    chore: update the convertion function

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,9 +2,7 @@
 
 /**
  * binary_to_uint - Converts a binary number to an unsigned int.
- *
  * @b: input binary number.
- *
  * Return: 0 if b is NULL or contains chars not 0 or 1 . . .
  *         else . . . it returns the converted number.
  */
