93a349264c10e173931fcf4adcaff6ec845f2953
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-04-09 00:49:06 +0300

    patch: update the binary change function

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,13 +19,15 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
 		c_num <<= 1;
 		if (b[i] == '1')
 			c_num += 1;
-		else if (b[i] != '0')
-			return (0);
-		else
-			return (0);
 	}
 	return (c_num);
 }
