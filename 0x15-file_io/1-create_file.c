fdbf4165f5610fbc595197238b4fc1cb4eac5fb4
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-04-09 03:15:12 +0300

    chore: create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,25 +10,26 @@
  * Return: (1) if successful . . . else . . . (-1).
  */
 
-int create_file(const char *filename, const char *text_content)
+int create_file(const char *filename, char *text_content)
 {
-	if (filename == NULL)
-	{
-		return (-1);
-	}
-
-	FILE *fp = fopen(filename, "w");
-
-	if (fp == NULL)
-	{
-		return (-1);
-	}
+	int sm_o, sm_w, sm_len = 0;
 
 	if (text_content != NULL)
 	{
-		fprintf(fp, "%s", text_content);
+		for (sm_len = 0; text_content[sm_len];)
+			sm_len++;
 	}
 
-	fclose(fp);
-	return (0);
+	if (filename == NULL)
+		return (-1);
+
+	sm_o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	sm_w = write(sm_o, text_content, sm_len);
+
+	if (sm_o == -1 || sm_w == -1)
+		return (-1);
+
+	close(sm_o);
+
+	return (1);
 }
