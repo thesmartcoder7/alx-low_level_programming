9111e4d67d720f7ec73eed79a4399318393fab15
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-04-09 03:42:12 +0300

    chore: copy information from one file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,16 +5,16 @@
 /**
  * main - this function copies info from file_from to file_to.
  *
- * @ac: number of arguments
- * @av: array of arguments
+ * @ac: the number of arguments
+ * @av: the array of arguments
  *
- * Return: Always 0.
+ * Return: 0.
  */
 
 int main(int ac, char **av)
 {
 	int file_from, file_to;
-	ssize_t  l_read = 1024, l_write, close_file;
+	ssize_t  sm_read = 1024, sm_write, sm_close;
 	char content[1024];
 
 	if (ac != 3)
@@ -31,25 +31,25 @@ int main(int ac, char **av)
 	{ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99); }
 
-	while (l_read == 1024)
+	while (sm_read == 1024)
 	{
-		l_read = read(file_from, content, 1024);
-		if (l_read == -1)
+		sm_read = read(file_from, content, 1024);
+		if (sm_read == -1)
 		{ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			exit(98); }
-		l_write = write(file_to, content, l_read);
-		if (l_write == -1)
+		sm_write = write(file_to, content, sm_read);
+		if (sm_write == -1)
 		{ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99); }
 	}
 
-	close_file = close(file_from);
-	if (close_file == -1)
+	sm_close = close(file_from);
+	if (sm_close == -1)
 	{ dprintf(STDERR_FILENO, "Error: Can't close fd  %d\n", file_from);
 		exit(100); }
 
-	close_file = close(file_to);
-	if (close_file == -1)
+	sm_close = close(file_to);
+	if (sm_close == -1)
 	{ dprintf(STDERR_FILENO, "Error: Can't close fd  %d\n", file_to);
 		exit(100); }
 	return (0);
