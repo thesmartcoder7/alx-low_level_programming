9da36330a9359bc69abf657dbf725a54a65c4b96
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-04-11 05:14:31 +0300

    chore: display the info contained in the ELF header at the start of an ELF file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -6,6 +6,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* function prototypes */
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int sm_elf);
+
 /**
  * main - this program the information contained in the
  *        ELF header at the start of an ELF file.
