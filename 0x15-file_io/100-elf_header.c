faba8b80a10eb62f1e3ed9f8074b3faf0579630e
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-04-11 03:37:16 +0300

    chore: display the info contained in the ELF header at the start of an ELF file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -125,19 +125,24 @@ void print_class(unsigned char *e_ident)
 {
 	printf("  Class:                             ");
 
-	switch (e_ident[EI_CLASS])
+	for (int i = 0; i < sizeof(e_ident); i++)
 	{
-	case ELFCLASSNONE:
-		printf("none\n");
-		break;
-	case ELFCLASS32:
-		printf("ELF32\n");
-		break;
-	case ELFCLASS64:
-		printf("ELF64\n");
-		break;
-	default:
-		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+		if (i == EI_CLASS)
+		{
+			switch (e_ident[i]) {
+				case ELFCLASSNONE:
+					printf("none\n");
+					break;
+				case ELFCLASS32:
+					printf("ELF32\n");
+					break;
+				case ELFCLASS64:
+					printf("ELF64\n");
+					break;
+				default:
+					printf("<unknown: %x>\n", e_ident[i]);
+			}
+		}
 	}
 }
 
