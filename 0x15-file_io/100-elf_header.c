0c86b0734d61b5a9f40e02f6048ace3e76b301cc
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-04-11 03:33:43 +0300

    chore: display the info contained in the ELF header at the start of an ELF file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -30,10 +30,10 @@ void close_elf(int elf);
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	Elf64_Ehdr *header;
-	int o, r;
+	int sm_open, sm_read;
 
-	o = open(argv[1], O_RDONLY);
-	if (o == -1)
+	sm_open = open(argv[1], O_RDONLY);
+	if (sm_open == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
@@ -41,15 +41,15 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	header = malloc(sizeof(Elf64_Ehdr));
 	if (header == NULL)
 	{
-		close_elf(o);
+		close_elf(sm_open);
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
 	}
-	r = read(o, header, sizeof(Elf64_Ehdr));
-	if (r == -1)
+	sm_read = read(sm_open, header, sizeof(Elf64_Ehdr));
+	if (sm_read == -1)
 	{
 		free(header);
-		close_elf(o);
+		close_elf(sm_open);
 		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
 		exit(98);
 	}
@@ -66,7 +66,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	print_entry(header->e_entry, header->e_ident);
 
 	free(header);
-	close_elf(o);
+	close_elf(sm_open);
 	return (0);
 }
 
