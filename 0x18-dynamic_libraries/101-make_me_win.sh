6c5b694bf51efbcdde21c7d1d68c918bc3a5b09d
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-05-23 03:11:57 +0300

    chore: tweak the script and test locally

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/thesmartcoder7/alx-low_level_programming/main/0x18-dynamic_libraries/win_giga.so
-export LD_PRELOAD=/tmp/win_giga.so
+wget -P .. https://github.com/thesmartcoder7/alx-low_level_programming/raw/main/0x18-dynamic_libraries/win_giga.so
+export LD_PRELOAD=/$PWD/../win_giga.so
